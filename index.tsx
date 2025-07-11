a2043ba7435f4f6bee83113a7e767cc05315373e
kamil-crypto <bookstormpl@gmail.com>
2025-07-11 13:39:58 +0200

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>✅ Aplikacja działa!</h1>
+      <p>To jest przykładowa strona startowa.</p>
+    </div>
+  );
+}
