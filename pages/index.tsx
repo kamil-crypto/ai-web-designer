10e7b044cb1e9cd60415a2661feb82230ab08880
kamil-crypto <bookstormpl@gmail.com>
2025-07-11 23:21:19 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '100px', fontSize: '24px' }}>
+      👋 Witaj w Twojej aplikacji AI do projektowania stron!
+    </div>
+  );
+}
