0a60b5508fc7506db51997520bb531aa849aca90
kamil-crypto <bookstormpl@gmail.com>
2025-07-11 13:47:21 +0200

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>✅ Aplikacja działa!</h1>
-      <p>To jest przykładowa strona startowa.</p>
-    </div>
-  );
-}
